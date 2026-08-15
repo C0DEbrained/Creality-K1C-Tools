394743828550de5f0545cdda620ede98d7075a9b
Corey <1339555+C0DEbrained@users.noreply.github.com>
2026-08-15 16:36:33 +0100

    Clean up install_entware.sh script
    Remove commented-out checks for paths.sh and OPKG installation.

--- scripts/install_entware.sh
+++ scripts/install_entware.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-#if [ ! -f paths.sh ]; then
-#  echo "Paths file not found. Exiting!" && exit 1
-#fi
-#
-#. paths.sh
-
 DATA=/usr/data
 APPETC=/usr/apps/etc
 INITD=$APPETC/init.d
 OPT_FILE_NAME=entware_opt_mount.img
+
+if (command -v opkg > /dev/null); then
+  echo "OPKG is already installed!"
+  exit 1
+fi
 
 if [ -f $DATA/$OPT_FILE_NAME ]; then
   echo "Existing $DATA/$OPT_FILE_NAME file found. Skipping creation."
